196151d5eeb79de01a3301c81de2954b0979204d
ajayhao <ajay.hao@gmail.com>
2020-02-28 16:51:28 +0800

    功能优化

--- GtjaOATool/oamain.py
+++ GtjaOATool/oamain.py
@@ -3,6 +3,7 @@ from pyquery import PyQuery as pq
 from tkinter import ttk
 from tkinter import messagebox
 from tkinter import *
+import datetime
 import time
 import requests
 import csv
@@ -10,6 +11,7 @@ import csv
 # 外部传入参数
 mainframe = None
 begin_date_sv = None
+leader_date_sv = None
 end_date_sv = None
 session_id_sv = None
 file_type_sv = None
@@ -17,21 +19,22 @@ need_detail_sv = None
 output_path_sv = None
 canvas = None
 process_sv = None
+button = None
 output_path = ''
 ltpa_token_string = ''
 file_type = ''
-CANVAS_LENGTH = 450
 is_detail_output = False
-currentPage = 1
-pageSize = 1000
 oa_headers = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36',
     'Host': 'oa.gtja.net'
 }
 
+CANVAS_LENGTH = 600
+PAGE_SIZE = 100
 
-# 抓取合同协议列表
-def get_contract_list():
+
+# 分页取数
+def get_contract_list_by_page(page_no):
     headers = {
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36',
         'Content-Type': 'application/x-www-form-urlencoded'
@@ -50,8 +53,8 @@ def get_contract_list():
             },
             "Type": "OA_TRANS_Q9902",
             "Page": {
-                "CurrentPage": currentPage,
-                "PageSize": pageSize
+                "CurrentPage": page_no,
+                "PageSize": PAGE_SIZE
             },
             "ColOrder": {
                 "ColName": "",
@@ -75,9 +78,23 @@ def get_contract_list():
         # test
         #write_file(fullpath=output_path+'列表返回结果.json', text=response.text)
         return response.json()
-        # for key, value in response.cookies.items():
-        #     print(key + '=' + value)
     return None
+
+
+# 抓取合同协议列表
+def get_contract_list():
+    total_list = []
+    resp = get_contract_list_by_page(1)
+    page_info = resp['Page']
+    #total_records = page_info['Records']
+    total_page = page_info['TotalPage']
+    for page in range(1, total_page+1):
+        resp = get_contract_list_by_page(page)
+        batch_data = resp['Data']
+        if batch_data is not None and len(batch_data) > 0:
+            total_list = total_list + batch_data
+
+    return total_list
 
 
 # 抓取合同详情
@@ -176,43 +193,66 @@ def execute():
     global output_path
     global is_detail_output
     global file_type
-    # 先重置进度条
-    reset_progress()
 
-    ltpa_token_string = ltpa_token_text.get(0.0, "end").strip().replace('\n', '').replace('\r', '')
-    output_path = output_path_sv.get()
-    if not output_path.endswith('/') and not output_path.endswith('\\'):
-        output_path = output_path + '/'
-    print(need_detail_sv.get())
-    if need_detail_sv.get() == 1:
-        is_detail_output = True
+    # 参数校验
+    if begin_date_sv.get() == '' or \
+            end_date_sv.get() == '' or \
+            leader_date_sv.get() == '' or \
+            session_id_sv.get() == '' or \
+            output_path_sv.get() == '' or \
+            ltpa_token_text.get(0.0, "end").replace('\n', '') == '':
+        messagebox.showwarning("执行失败","解析异常：输入项未填写完整")
+        return
 
-        # 查询文件类型
-    if file_type_sv.get() == 1:
-        file_type = 'OA006'
-    else:
-        file_type = 'OA012'
-    list_resp = get_contract_list()
-    contract_list = list_resp['Data']
-    #it = iter(contract_list)
-    # 需要对excel去重
-    excel_dict = {}
-    idx = 0
-    total = len(contract_list)
-    while idx < total:
-        item = contract_list[idx]
-        ret_tuple = get_contract_detail(item)
-        get_process_track(item, ret_tuple)
-        if ret_tuple['jwzfcsj'] != '':
-            excel_dict[ret_tuple['title']] = ret_tuple
-        refresh_progress(idx+1, total, ret_tuple['title'])
-        idx += 1
-    # test
-    #write_file(output_path + 'finalData.json', str(excel_list))
-    # 输出excel
-    export_as_csv(list(excel_dict.values()))
-    messagebox.showinfo("执行完毕", "解析完毕，请到下载目录查看结果")
+    try:
+        button.state(["disabled"])
+        # 先重置进度条
+        reset_progress()
+        # 参数解析
+        ltpa_token_string = ltpa_token_text.get(0.0, "end").strip().replace('\n', '').replace('\r', '')
+        output_path = output_path_sv.get()
+        if not output_path.endswith('/') and not output_path.endswith('\\'):
+            output_path = output_path + '/'
+        if need_detail_sv.get() == 1:
+            is_detail_output = True
+            # 查询文件类型
+        if file_type_sv.get() == 1:
+            file_type = 'OA006'
+        else:
+            file_type = 'OA012'
+        leader_date_str = leader_date_sv.get()
 
+        # 获得申请列表
+        contract_list = get_contract_list()
+        #list_resp = get_contract_list()
+        #contract_list = list_resp['Data']
+
+        # 需要对excel去重
+        #excel_dict = {}
+        excel_list = []
+        idx = 0
+        total = len(contract_list)
+        while idx < total:
+            item = contract_list[idx]
+            ret_tuple = get_contract_detail(item)
+            get_process_track(item, ret_tuple)
+            if ret_tuple['jwzfcsj'] != '' and ret_tuple['jwzfcsj'] != '未结束':
+                #excel_dict[ret_tuple['title']] = ret_tuple
+                # 判断江伟总时间是否落在这个区间  "%Y-%m-%d %H:%M:%S"
+                t1 = datetime.datetime.strptime(ret_tuple['jwzfcsj'],'%Y-%m-%d %H:%M:%S')
+                jwzfcsj = t1.strftime("%Y%m")
+                if jwzfcsj == leader_date_str:
+                    excel_list.append(ret_tuple)
+            refresh_progress(idx+1, total, ret_tuple['title'])
+            idx += 1
+        # test
+        #write_file(output_path + 'finalData.json', str(excel_list))
+        # 输出excel
+        #excel_list = list(excel_dict.values())
+        export_as_csv(excel_list)
+        messagebox.showinfo("执行完毕", "解析完毕，请到下载目录查看结果")
+    finally:
+        button.state(["!disabled"])
 
 # 版本信息
 def show_version_info():
@@ -237,10 +277,13 @@ def draw_frame(root):
     global need_detail_sv
     global ltpa_token_text
     global output_path_sv
+    global leader_date_sv
     global canvas
     global process_sv
+    global button
     begin_date_sv = StringVar()
     end_date_sv = StringVar()
+    leader_date_sv = StringVar()
     session_id_sv = StringVar()
     file_type_sv = IntVar()
     need_detail_sv = IntVar()
@@ -248,48 +291,61 @@ def draw_frame(root):
     process_sv = StringVar()
     root.columnconfigure(0, weight=1)
     root.rowconfigure(0, weight=1)
-    mainframe = Frame(root, height=5, width=480) #bg='black'
+    mainframe = Frame(root) #bg='black'
     mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
     # 组件
-    readme = Label(mainframe, bg='white', text='操作说明：参数均为必填项，其中oem.sessionid与LtpaToken通过以下方式获取：使用特定人员账号在Chrome浏览器上登录OA，并点击个人工作台。等待办展现后，按下F12，选Application标签页，点开左侧Storage->Cookies->https://link.gtja.net，找到这两个名称对应的值(Value)拷贝出即可', anchor='center', wraplength=500, justify='left')
-    begin_date_label = Label(mainframe, text='开始日期(yyyyMMdd):')
-    end_date_label = Label(mainframe, text='结束日期(yyyyMMdd):')
-    begin_date_entry = ttk.Entry(mainframe, textvariable=begin_date_sv)
-    end_date_entry = ttk.Entry(mainframe, textvariable=end_date_sv)
+    readme = Label(mainframe, bg='white', text='操作说明：参数均为必填项，其中oem.sessionid与LtpaToken通过以下方式获取：使用特定人员账号在Chrome浏览器上登录OA，并点击个人工作台。等待办展现后，按下F12，选Application标签页，点开左侧Storage->Cookies->https://link.gtja.net，找到这两个名称对应的值(Value)拷贝出即可', anchor='center', wraplength=780, justify='left')
+    begin_date_label = Label(mainframe, text='开始日期(yyyyMMdd):', justify='right')
+    end_date_label = Label(mainframe, text='结束日期(yyyyMMdd):', justify='right')
+    begin_date_entry = ttk.Entry(mainframe, textvariable=begin_date_sv, width=20)
+    end_date_entry = ttk.Entry(mainframe, textvariable=end_date_sv, width=20)
     session_id_label = Label(mainframe, text='oem.sessionid:')
     ltpa_token_label = Label(mainframe, text='LtpaToken:')
-    output_path_label = Label(mainframe, text='文件输出完整路径(确保已存在):', wraplength=96, justify='right')
+    output_path_label = Label(mainframe, text='结果输出完整路径(确保目录已存在):', wraplength=96, justify='right')
     process_label = Label(mainframe, textvariable=process_sv)
     session_id_entry = ttk.Entry(mainframe, textvariable=session_id_sv)
-    ltpa_token_text = Text(mainframe, height=7, width=1)
+    ltpa_token_text = Text(mainframe, height=5, width=1)
     r1 = Radiobutton(mainframe, text="合同协议", value=1, variable=file_type_sv)
     r2 = Radiobutton(mainframe, text="印章申请", value=2, variable=file_type_sv)
-    c1 = Checkbutton(mainframe, text="是否打印详单(长耗时)", wraplength=74, justify='right', variable=need_detail_sv)
+    c1 = Checkbutton(mainframe, text="是否下载详情(将明细文件保存至输出路径)", variable=need_detail_sv)
     output_path_entry = ttk.Entry(mainframe, textvariable=output_path_sv)
     #按钮
     button = ttk.Button(mainframe, text="确定", command=execute)
     #布局
-    readme.grid(row=0, columnspan=4, sticky=(W,N,E))
-    begin_date_label.grid(row=1, column=0, sticky=E)
-    begin_date_entry.grid(row=1, column=1, sticky=(W,E))
-    end_date_label.grid(row=1, column=2, sticky=E)
-    end_date_entry.grid(row=1, column=3, sticky=(W,E))
-    session_id_label.grid(row=2, column=0, sticky=E)
-    session_id_entry.grid(row=2, column=1, columnspan=3, sticky=(W,E))
-    ltpa_token_label.grid(row=3, column=0, sticky=E)
-    ltpa_token_text.grid(row=3, column=1, columnspan=3, sticky=(W,E))
-    output_path_label.grid(row=4, column=0, sticky=E)
-    output_path_entry.grid(row=4, column=1, columnspan=3, sticky=(W,E))
-    c1.grid(row=5, column=0, sticky=E)
-    Label(mainframe, text='选择文案类型:').grid(row=5, column=1, sticky=E)
-    r1.grid(row=5, column=2, sticky=E)
-    r2.grid(row=5, column=3, sticky=W)
+    row_at = 0
+    readme.grid(row=row_at, columnspan=6, sticky=(W,N,E))
+    row_at += 1
+    begin_date_label.grid(row=row_at, column=0, sticky=E)
+    begin_date_entry.grid(row=row_at, column=1, sticky=W)
+    end_date_label.grid(row=row_at, column=2, sticky=E)
+    end_date_entry.grid(row=row_at, column=3, sticky=W)
+    Label(mainframe, text='江伟总审批通过时间(yyyyMM):', wraplength=110, justify='right').grid(row=row_at, column=4, sticky=E)
+    ttk.Entry(mainframe, textvariable=leader_date_sv, width=10).grid(row=row_at, column=5, sticky=(W,E))
+
+    row_at += 1
+    Label(mainframe, text='选择文案类型:').grid(row=row_at, column=0, sticky=E)
+    r1.grid(row=row_at, column=1, sticky=E)
+    r2.grid(row=row_at, column=2, sticky=W)
+    c1.grid(row=row_at, column=3, columnspan=3, sticky=W)
+
+    row_at += 1
+    session_id_label.grid(row=row_at, column=0, sticky=E)
+    session_id_entry.grid(row=row_at, column=1, columnspan=5, sticky=(W,E))
+    row_at += 1
+    ltpa_token_label.grid(row=row_at, column=0, sticky=E)
+    ltpa_token_text.grid(row=row_at, column=1, columnspan=5, sticky=(W,E))
+    row_at += 1
+    output_path_label.grid(row=row_at, column=0, sticky=E)
+    output_path_entry.grid(row=row_at, column=1, columnspan=5, sticky=(W,E))
+    row_at += 1
     # 设置下载进度条
-    Label(mainframe, text='解析进度:').grid(row=6, column=0, sticky=E)
+    Label(mainframe, text='解析进度:').grid(row=row_at, column=0, sticky=E)
     canvas = Canvas(mainframe, width=CANVAS_LENGTH, height=22, bg="white")
-    canvas.grid(row=6, column=1, columnspan=3, sticky=(W,E))
-    process_label.grid(row=7, column=0, columnspan=4, sticky=(W,E))
-    button.grid(row=8, column=3, sticky=E)
+    canvas.grid(row=row_at, column=1, columnspan=4, sticky=(W,E))
+    row_at += 1
+    process_label.grid(row=row_at, column=1, columnspan=4, sticky=(W,E))
+    row_at += 1
+    button.grid(row=row_at, column=2, columnspan=2)
     for child in mainframe.winfo_children():
         child.grid_configure(padx=5, pady=5)
     # 初始化
@@ -298,6 +354,7 @@ def draw_frame(root):
     #临时
     begin_date_sv.set('20200101')
     end_date_sv.set('20200131')
+    leader_date_sv.set('202001')
 
 
 # 恢复进度条
@@ -324,7 +381,7 @@ def refresh_progress(cnt, total, title):
 def ui_frame():
     win = Tk()
     win.title('产品组OA绩效抓取小工具')
-    win.geometry("620x500+300+100")
+    win.geometry("840x430+300+100")
     win.resizable(0, 0) # 窗口大小固定
     draw_menu(win)
     draw_frame(win)
