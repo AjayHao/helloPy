f017ed0cae0c2e03b763ce2f3874b455dd41d29f
ajayhao <ajay.hao@gmail.com>
2020-08-04 22:51:02 +0800

    多线程

--- demos/datamatrix/education/school.py
+++ demos/datamatrix/education/school.py
@@ -1,8 +1,9 @@
-import uuid
-from pyquery import PyQuery as pq
-
 import datetime
+import uuid
+from concurrent.futures import ThreadPoolExecutor
+
 import requests
+from pyquery import PyQuery as pq
 
 from demos.datamatrix.education.model.schoolmodel import School, DBSession
 
@@ -25,17 +26,21 @@ def get_school_urls_by_city(cityno):
     return list(r)
 
 
-def get_total_pages(url):
-    response = requests.get(url=url)
+def get_total_pages(url, typeno):
+    page_url = url + '&t=' + typeno
+    response = requests.get(url=page_url)
     htmlstr = response.text
     htmlobj = pq(htmlstr)
     page_a = htmlobj('div.fy a:last')
     max_page_url = page_a.attr('href')
-    lastidx = max_page_url.rindex('=')
-    return int(max_page_url[lastidx+1:])
+    if max_page_url is not None:
+        lastidx = max_page_url.rindex('=')
+        return int(max_page_url[lastidx+1:])
+    else:
+        return 0
 
 
-def get_school_detail(schoolurl):
+def get_school_detail(schoolurl, typeno):
     def parse_txt(str):
         return str.split('：')[1].strip()
     school = {
@@ -63,8 +68,12 @@ def get_school_detail(schoolurl):
             school['edu_type'] = parse_txt(prop.text())
         if propname == '学校级别':
             school['level'] = parse_txt(prop.text())
-        if propname == '学校类型':
-            school['type'] = parse_txt(prop.text())
+        if typeno == xiaoxue:
+            school['type'] = '小学'
+        elif typeno == youeryuan:
+            school['type'] = '幼儿园'
+        elif typeno == chuzhong:
+            school['type'] = '初中'
         if propname == '学校地址':
             school['address'] = parse_txt(prop.text())
     intro_div = outer_div.find('div.jj>p')
@@ -99,19 +108,26 @@ def get_school_urls_by_page(city_url, typeno, pageno):
 
 
 def pull_schools(cityno, typeno):
+    def get_and_insert(school_url, typeno):
+        schoolobj = get_school_detail(school_url, typeno)
+        try:
+            add_school(schoolobj)
+        except:
+            print(schoolobj)
+
     city_urls = get_school_urls_by_city(cityno)
+    thread_pool = ThreadPoolExecutor(max_workers=4, thread_name_prefix="school_")
     for city_url in city_urls:
-        max_page = get_total_pages(city_url)
+        max_page = get_total_pages(city_url, typeno)
         for i in range(1, max_page+1):
             school_urls = get_school_urls_by_page(city_url, typeno, i)
             for school_url in school_urls:
-                schoolobj = get_school_detail(school_url)
-                try:
-                    add_school(schoolobj)
-                except:
-                    print(schoolobj)
+                thread_pool.submit(get_and_insert, school_url, typeno)
+
+    thread_pool.shutdown(wait=True)
+
 
 if __name__ == '__main__':
     cityno = shanghai
-    typeno = xiaoxue
+    typeno = chuzhong
     pull_schools(cityno, typeno)
